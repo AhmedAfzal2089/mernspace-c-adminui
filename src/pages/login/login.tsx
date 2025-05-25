ee1699371f42dc01640302c1f3b7784a13b667fb
AhmedAfzal2089 <ameafzal@gmail.com>
2025-05-25 12:17:00 +0500

    add login form

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,13 +1,92 @@
+import { Card, Layout, Space, Form, Input, Checkbox, Button, Flex } from "antd";
+import { LockFilled, LockOutlined, UserOutlined } from "@ant-design/icons";
+import Logo from "../../components/icons/Logo";
 const LoginPage = () => {
   return (
     <>
-      <h1>Sign in</h1>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Log in</button>
-      <label htmlFor="rememeber-me">Remember me</label>
-      <input type="checkbox" id="rememeber-me" />
-      <a href="#">Forgot password</a>
+      <Layout
+        style={{ height: "100vh", display: "grid", placeItems: "center" }}
+      >
+        <Space direction="vertical" align="center" size="large">
+          <Layout.Content
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Logo />
+          </Layout.Content>
+          <Card
+            bordered={false}
+            style={{ width: 300 }}
+            title={
+              <Space
+                style={{
+                  width: "100%",
+                  fontSize: 16,
+                  justifyContent: "center",
+                }}
+              >
+                <LockFilled />
+                Sign in
+              </Space>
+            }
+          >
+            <Form
+              initialValues={{
+                username: "test",
+                password: "secret",
+                remember: true,
+              }}
+            >
+              <Form.Item
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please Input Your Username",
+                  },
+                  { type: "email", message: "Email is not Valid" },
+                ]}
+              >
+                <Input prefix={<UserOutlined />} placeholder="Username" />
+              </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please input your password",
+                  },
+                ]}
+              >
+                <Input.Password
+                  prefix={<LockOutlined />}
+                  placeholder="Password"
+                />
+              </Form.Item>
+              <Flex justify="space-between">
+                <Form.Item name="remember" valuePropName="checked">
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+                <a href="" id="login-form-forgot">
+                  Forgot password
+                </a>
+              </Flex>
+              <Form.Item name="password">
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  style={{ width: "100% " }}
+                >
+                  Log in
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+        </Space>
+      </Layout>
     </>
   );
 };
