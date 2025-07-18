a63aa7e0b55f8a5628700a5691238340f2cf2051
AhmedAfzal2089 <ameafzal@gmail.com>
2025-07-18 16:51:11 +0500

    add sidebar layout

---
+++ src/pages/users/Users.tsx
@@ -0,0 +1,5 @@
+const Users = () => {
+  return <div>YSersss</div>;
+};
+
+export default Users;
--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Users from "./pages/users/Users";
 import LoginPage from "./pages/login/login";
 import Dashboard from "./layouts/Dashboard";
 import NonAuth from "./layouts/NonAuth";
@@ -17,6 +18,10 @@ export const router = createBrowserRouter([
           {
             path: "",
             element: <HomePage />,
+          },
+          {
+            path: "/users",
+            element: <Users />,
           },
         ],
       },
