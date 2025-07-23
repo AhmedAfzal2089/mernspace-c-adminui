560e996d4f3b13a6d2baaf9b923166546b80337d
AhmedAfzal2089 <ameafzal@gmail.com>
2025-07-23 22:43:27 +0500

    add users drawer

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,12 +1,44 @@
-import { Breadcrumb, Table, Space } from "antd";
-import { RightOutlined } from "@ant-design/icons";
+import { Breadcrumb, Table, Space, Drawer, Button } from "antd";
+import { PlusOutlined, RightOutlined } from "@ant-design/icons";
 import { Link, Navigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { getUsers } from "../../http/api";
 import { User } from "../../types";
 import { useAuthStore } from "../../store";
 import UsersFilter from "./UsersFilter";
+import { useState } from "react";
+const columns = [
+  {
+    title: "ID",
+    dataIndex: "id",
+    key: "id",
+  },
+  {
+    title: "Name",
+    dataIndex: "firstName",
+    key: "firstName",
+    render: (_text: string, record: User) => {
+      return (
+        <div>
+          {record.firstName} {record.lastName}
+        </div>
+      );
+    },
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Role",
+    dataIndex: "role",
+    key: "role",
+  },
+];
 const Users = () => {
+  const [DrawerOpen, setDrawerOpen] = useState(false);
+
   const {
     data: users,
     isLoading,
@@ -23,35 +55,6 @@ const Users = () => {
     return <Navigate to="/" replace={true} />;
   }
 
-  const columns = [
-    {
-      title: "ID",
-      dataIndex: "id",
-      key: "id",
-    },
-    {
-      title: "Name",
-      dataIndex: "firstName",
-      key: "firstName",
-      render: (_text: string, record: User) => {
-        return (
-          <div>
-            {record.firstName} {record.lastName}
-          </div>
-        );
-      },
-    },
-    {
-      title: "Email",
-      dataIndex: "email",
-      key: "email",
-    },
-    {
-      title: "Role",
-      dataIndex: "role",
-      key: "role",
-    },
-  ];
   return (
     <>
       <Space direction="vertical" size="large" style={{ width: "100%" }}>
@@ -65,8 +68,36 @@ const Users = () => {
           onFilterChange={(filterName: string, filterValue: string) => {
             console.log(filterName, filterValue);
           }}
-        />
+        >
+          <Button
+            type="primary"
+            icon={<PlusOutlined />}
+            onClick={() => setDrawerOpen(true)}
+          >
+            Add User
+          </Button>
+        </UsersFilter>
         <Table columns={columns} dataSource={users} rowKey="id" />
+        <Drawer
+          title="Create User"
+          width={720}
+          open={DrawerOpen}
+          destroyOnHidden={true}
+          onClose={() => {
+            setDrawerOpen(false);
+          }}
+          extra={
+            <Space>
+              <Button>Cancel</Button>
+              <Button type="primary">Submit</Button>
+            </Space>
+          }
+        >
+          <p>Some Content </p>
+          <p>Some Content </p>
+          <p>Some Content </p>
+          <p>Some Content </p>
+        </Drawer>
       </Space>
     </>
   );
