50d3e823410c790575c709d1fb8d196460edcc68
AhmedAfzal2089 <ameafzal@gmail.com>
2025-07-24 13:01:51 +0500

    add tenants table, drawer

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/login/login";
 import Dashboard from "./layouts/Dashboard";
 import NonAuth from "./layouts/NonAuth";
 import Root from "./layouts/Root";
+import Resturants from "./pages/resturants/Tenants";
 
 export const router = createBrowserRouter([
   {
@@ -22,6 +23,10 @@ export const router = createBrowserRouter([
           {
             path: "/users",
             element: <Users />,
+          },
+          {
+            path: "/restaurants",
+            element: <Resturants />,
           },
         ],
       },
