928f9d4434796714a230e3292f6795212e459f3e
AhmedAfzal2089 <ameafzal@gmail.com>
2025-08-04 10:57:56 +0500

    add breadcrumb router for products

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ import Dashboard from "./layouts/Dashboard";
 import NonAuth from "./layouts/NonAuth";
 import Root from "./layouts/Root";
 import Resturants from "./pages/resturants/Tenants";
+import Products from "./pages/products/Products";
 
 export const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ export const router = createBrowserRouter([
           {
             path: "/restaurants",
             element: <Resturants />,
+          },
+          {
+            path: "/products",
+            element: <Products />,
           },
         ],
       },
